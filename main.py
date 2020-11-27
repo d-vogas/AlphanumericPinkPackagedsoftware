9aae4057989a1be486ca6635c8a1614e673f358e
dvogas <dvogas13@gmail.com>
2020-11-27 11:30:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+x={1,4,8}
+x|{1,3}; x&{2}
+print (x|{1,3})
+print (x&{2})
+george={"id":2, "age":25}
+print (george['id'])
+print (george['age'])
+def oddnum(x):
+  if x%2==0:
+    return True
+  else:
+    return False
+print(oddnum(2))
+import math
+print(math.e)
+print(math.pi)
+import scipy.constants
+print(scipy.constants.k)
